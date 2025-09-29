5e997443d124fd15538c2a841353a9303ba775f1
brycebozz <brycebozz@gmail.com>
2025-09-29 17:55:53 -0600

    Updated to add if statement incase filter not in list and added comments.

--- Advanced Function and Logic/function_filter_list.py
+++ Advanced Function and Logic/function_filter_list.py
@@ -1,13 +1,12 @@
 def filter_list(argument_list, filter):
     new_list = []
     while True:
-        for x in argument_list:
-            if x == filter:
-                new_list.append(x)
-                return new_list
-            else:
-                new_list.append(x)
-
-
-test = [1,2,3,4,5,6,7,8,9]
-print(filter_list(test,6))
+        if filter in argument_list: #checking to see if filter is in list so it doesnt loop forever if it isn't found
+            for x in argument_list: #if there is a filter it starts looping until it finds the filter
+                if x == filter:
+                    new_list.append(x) #need to have filter in the list according to instructions example
+                    return new_list
+                else:
+                    new_list.append(x)
+        else:
+            return "Filter not found in list." #returns message telling that fileter is not in the lsit
