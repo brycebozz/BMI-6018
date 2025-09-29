ac3f11e3bb288d475086d9be1ca7808730a247af
brycebozz <brycebozz@gmail.com>
2025-09-29 02:08:56 -0600

    Update while_inner_plus.py

--- Advanced Function and Logic/while_inner_plus.py
+++ Advanced Function and Logic/while_inner_plus.py
@@ -3,9 +3,9 @@ def innermost_list_plus1(argument_list):
     while True: #Infinite while loop wont stop unless it hits break
         innermost = False #You have to assume it is not innermost
         for x in innermost_list:
-            if type(x) == list:
+            if type(x) == list: #sees if the types match
                 innermost_list = x #makes innermost_list current list found
-                innermost = True
+                innermost = True #true put in so when it goes to line 10 it does not end the loop
                 break
         if innermost == False: #breaks from while loop if out of values in list and if innermost is false if it got to this line and innermost was true while loop would begin again.
             break
